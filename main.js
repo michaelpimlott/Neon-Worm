0cf1fddaf00a79f634d28b5251c48f38c3d92ea2
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-08-09 22:10:50 -0700

    add circle and square functions

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var heightInBlocks = height/blockSize;
 var score = 0;
 
 var drawBorder = function() {
-  ctx.fillStyle = 'Gray';
+  ctx.fillStyle = '#F408B8';
   ctx.fillRect(0, 0, width, blockSize);
   ctx.fillRect(0, height - blockSize, width, blockSize);
   ctx.fillRect(0, 0, blockSize, height);
@@ -34,6 +34,30 @@ var gameOver = function() {
   ctx.textBaseline = "middle";
   ctx.fillText("I love you, but you blew it. Game Over.", width/2, height/2);
 };
+
+var Block = function(col, row) {
+  this.col = col;
+  this.row = row;
+};
+
+Block.prototype.drawSquare = function(color) {
+  var x = this.col * blockSize;
+  var y = this.row * blockSize;
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, blockSize, blockSize);
+};
+
+Block.prototype.drawCircle = function(color) {
+  var centerX = this.col * blockSize + blockSize/2;
+  var centerY = this.row * blockSize + blockSize/2;
+  ctx.fillStyle = color;
+  circle(centerX, centerY, blockSize/2, true);
+};
+
+Block.prototype.equal = function(otherBlock) {
+  return this.col === otherBlock.col && this.row === otherBlock.row;
+};
+
 
 drawBorder();
 drawScore();
